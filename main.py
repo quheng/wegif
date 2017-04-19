38d53e3b20ec8a7099183bdf18d9b4a1871df519
quheng <494862190@qq.com>
2017-04-19 20:42:20 +0800

    utring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #coding=utf8
 import itchat
+import os
 import requests
 import random
 import urllib.request
@@ -7,6 +8,7 @@ from itchat.content import *
 from scrapy.selector import Selector
 
 GIF_TRIGGER = '/g'
+TURING_TRIGGER = '/t'
 
 
 def gif_path(name):
@@ -33,11 +35,37 @@ def gif_process(spllited, receiver):
     itchat.send_image(gif_path(receiver), toUserName=receiver)
 
 
+def turing_process(spllited, receiver):
+    url = 'http://www.tuling123.com/openapi/api'
+    if len(spllited) < 2:
+        return
+
+    payload = {
+        'key': os.environ['turingKey'],
+        'info': ' '.join(spllited[1:]),
+        'userid': receiver[1:]
+    }
+
+    res = requests.post(url, data=payload)
+    resJson = res.json()
+    code = resJson['code']
+    if code == 100000:
+        itchat.send(resJson['text'], receiver)
+    elif code == 200000:
+        itchat.send(f"{resJson['text']}: {resJson['url']}", receiver)
+    elif code == 302000:
+        itchat.send('   '.join([f"{item['article']}: {item['detailurl']}" for item in resJson]))
+    elif code == 308000:
+        itchat.send(f"名称: {resJson['name']}, 简介：{resJson['info']}, 地址：{resJson['detailurl']}", receiver)
+
+
 def incoming_msg(msg):
     fromUserName = msg['FromUserName']
     spllited = msg['Text'].split(' ')
     if spllited[0] == GIF_TRIGGER:
         gif_process(spllited, fromUserName)
+    elif spllited[0] == TURING_TRIGGER:
+        turing_process(spllited, fromUserName)
 
 
 @itchat.msg_register(TEXT)
@@ -49,6 +77,6 @@ def text_reply(msg):
 def group_text_reply(msg):
     incoming_msg(msg)
 
-# itchat.auto_login(True)
-itchat.auto_login(True, enableCmdQR=True)
+itchat.auto_login(True)
+# itchat.auto_login(True, enableCmdQR=True)
 itchat.run()
