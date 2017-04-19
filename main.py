5fc632205635c565409bcd75ee9e80d81a9ef812
quheng <494862190@qq.com>
2017-04-19 19:32:09 +0800

    remove some log

--- main.py
+++ main.py
@@ -23,23 +23,19 @@ def get_image(key_word, receiver):
     image_list = Selector(text=r.content).xpath(xpath).extract()
     image_url = image_list[random.randint(0, len(image_list)-1)]
     urllib.request.urlretrieve(image_url, gif_path(receiver))
-    print(f'send image url: {image_url}')
 
 
 def gif_process(spllited, receiver):
     key_word = ''
     if len(spllited) > 1:
         key_word = spllited[1]
-    print(f'get key word {key_word}')
     get_image(key_word, receiver)
-    print(receiver)
     itchat.send_image(gif_path(receiver), toUserName=receiver)
 
 
 def incoming_msg(msg):
     fromUserName = msg['FromUserName']
     spllited = msg['Text'].split(' ')
-
     if spllited[0] == GIF_TRIGGER:
         gif_process(spllited, fromUserName)
 
@@ -53,6 +49,6 @@ def text_reply(msg):
 def group_text_reply(msg):
     incoming_msg(msg)
 
-
+# itchat.auto_login(True)
 itchat.auto_login(True, enableCmdQR=True)
 itchat.run()
